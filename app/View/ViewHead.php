519f8506e22134dbfeb0d95bc738b4b8c926cec6
felipeDesenvolvedor <felipedasilvasantos30@gmail.com>
2020-05-08 13:04:09 -0300

    alteraçoes de estrutura

---
+++ app/View/ViewHead.php
@@ -0,0 +1,22 @@
+<head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+        <link rel="icon" href="/View/img/icon-frutas.ico"/>
+
+        <link rel="stylesheet" href="../app/View/css/reset.css">
+        <link rel="stylesheet" href="../app/View/css/font-awesome.css"> 
+        <link rel="stylesheet" href="../app/View/css/ViewPainelLadoEsquerdoStyle.css">
+        <link rel="stylesheet" href="../app/View/css/ViewPainelLadoDireitoStyle.css">
+        <link rel="stylesheet" href="../app/View/css/ViewPacienteNovoForm.css">
+        <link rel="stylesheet" href="../app/View/css/ErrorMesage.css">
+        <link rel="stylesheet" href="../app/View/css/index.css">
+        <script src="../app/View/js/formPaciente.js"></script>
+
+        <style>
+            pre {
+                font-size: 16px;
+                line-height: 23px;
+            }
+        </style>
+    </head>
--- autoload.php
+++ autoload.php
@@ -1,12 +1,10 @@
 <?php
     spl_autoload_register(function (string $nomeCompletoDaClasse) {
             
-            $diretorioAtual = explode(DIRECTORY_SEPARATOR, $_SERVER['DOCUMENT_ROOT']);
-                unset($diretorioAtual[count($diretorioAtual) - 1]);
-            $path = implode(DIRECTORY_SEPARATOR, $diretorioAtual);
+            $path  = $_SERVER['DOCUMENT_ROOT'];
             $path .= DIRECTORY_SEPARATOR;
-        
-            $caminhoArquivo = str_replace('Nutricionista', $GLOBALS['path'].'src', $nomeCompletoDaClasse);
+                
+            $caminhoArquivo = str_replace('Nutricionista', $GLOBALS['path'].'app', $nomeCompletoDaClasse);
             $caminhoArquivo = str_replace('\\', DIRECTORY_SEPARATOR, $caminhoArquivo);
             $caminhoArquivo .= '.php';
             
