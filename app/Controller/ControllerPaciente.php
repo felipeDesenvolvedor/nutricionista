e3345acbe31e6b3ef9a4ac69c77c8bdbef2eab20
felipeDesenvolvedor <felipedasilvasantos30@gmail.com>
2020-05-13 21:10:05 -0300

    Model Paciente

--- app/Controller/ControllerPaciente.php
+++ app/Controller/ControllerPaciente.php
@@ -2,6 +2,7 @@
   namespace app\Controller; 
   
   use src\classes\ClassRoutes;
+  use app\Model\ModelPessoa;
   
   class ControllerPaciente
   { 
@@ -41,6 +42,32 @@
       $this->titulo = "Cadastro de paciente";
       require_once($GLOBALS['caminhoDosArquivos']['ViewMenuPainel']);
       require_once($GLOBALS['caminhoDosArquivos']['ViewInicioHTML']);
+    }
+
+    public function cadastrar() 
+    {
+        if(isset($_POST['nome']) && !empty($_POST['nome'])) {
+         new ModelPessoa([                                    
+                    "nome"           => filter_input(INPUT_POST, 'nome',           FILTER_SANITIZE_STRING), 
+                    "cpf"            => filter_input(INPUT_POST, 'cpf',            FILTER_SANITIZE_STRING),
+                    "rg"             => filter_input(INPUT_POST, 'rg',             FILTER_SANITIZE_STRING),
+                    "dataNascimento" => filter_input(INPUT_POST, 'dataNascimento', FILTER_SANITIZE_STRING),
+                    "sexo"           => filter_input(INPUT_POST, 'sexo',           FILTER_SANITIZE_STRING),
+                    "responsavel"    => filter_input(INPUT_POST, 'responsavel',    FILTER_SANITIZE_STRING),
+                    "cpfResponsavel" => filter_input(INPUT_POST, 'cpfResponsavel', FILTER_SANITIZE_STRING),   
+                    "CEP"            => filter_input(INPUT_POST, 'CEP',            FILTER_SANITIZE_STRING),   
+                    "endereco"       => filter_input(INPUT_POST, 'endereco',       FILTER_SANITIZE_STRING),   
+                    "numeroEndereco" => filter_input(INPUT_POST, 'numeroEndereco', FILTER_SANITIZE_STRING),   
+                    "municipio"      => filter_input(INPUT_POST, 'municipio',      FILTER_SANITIZE_STRING),   
+                    "bairro"         => filter_input(INPUT_POST, 'bairro',         FILTER_SANITIZE_STRING),   
+                    "complemento"    => filter_input(INPUT_POST, 'complemento',    FILTER_SANITIZE_STRING),   
+                    "telefone1"      => filter_input(INPUT_POST, 'telefone1',      FILTER_SANITIZE_STRING),   
+                    "telefone2"      => filter_input(INPUT_POST, 'telefone2',      FILTER_SANITIZE_STRING),   
+                    "email"          => filter_input(INPUT_POST, 'email',          FILTER_SANITIZE_STRING)   
+            ]);
+
+            header('Location:/pacientes', true, 302);
+        }
     }
   }
 ?>
--- app/Model/ModelPessoa.php
+++ app/Model/ModelPessoa.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace app;
+namespace app\Model;
 
 class ModelPessoa 
 {
