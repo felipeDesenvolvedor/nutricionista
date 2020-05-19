d4f87256bd8c11cb93db9290c2761f59b67dfde9
felipeDesenvolvedor <felipedasilvasantos30@gmail.com>
2020-05-19 18:41:58 -0300

    Grade pacientes

--- app/View/ViewPacienteAcoes.php
+++ app/View/ViewPacienteAcoes.php
@@ -1,23 +1,41 @@
 <?php 
         echo 
-        '
-        <div>
-            <form>
-                <input class="input-busca-paciente" type="text" name="" placeholder="Pesquisar por nome"/>
-                <input class="btn-filtro-paciente" type="button" name="" value="Filtros"/>
-                <input class="btn-busca-paciente" type="button" name="" value="Buscar"/>
-                <a href=/pacientes/novo><input class="btn-novo-paciente" type="button" name="" value="Novo"/></a>
-            </form>
-        </div>    
+        '   <div>
+                <form>
+                    <input class="input-busca-paciente" type="text" name="" placeholder="Pesquisar por nome"/>
+                    <input class="btn-filtro-paciente" type="button" name="" value="Filtros"/>
+                    <input class="btn-busca-paciente" type="button" name="" value="Buscar"/>
+                    <a href=/pacientes/novo><input class="btn-novo-paciente" type="button" name="" value="Novo"/></a>
+                </form>
+            </div>    
         ';
 
         echo '<div class="lista-pacientes">';
-        if(count($this->pacientes)) {
-            echo '<p>lista de pacientes</p>';     
-        }
-        else{
-            echo '<p>Voçê ainda não possui pacientes cadastrados, para cadastrar clique no botão abaixo</p>';
-            echo '<a href=/pacientes/novo><input class="btn-novo-paciente" type="button" name="" value="Novo"/></a>';
-        }
+            if(count($this->pacientes)) {
+                $indice = 0;
+
+                foreach($this->pacientes as $paciente)
+                {   
+                    $indice += 1;
+                    
+                    echo '<div class=lista-paciente>';
+                        echo '<div class="lista-paciente-foto"></div>';
+                        
+                        echo '<div class="lista-paciente-dados">';
+                            echo '<span>'.$paciente['nome'].'</span>';
+                            echo '<span>'.$paciente['sexo'].'</span>';
+                            echo '<span class="idade">'.$paciente['dataNascimento'].'</span>';
+                        echo '</div>';
+                    echo '</div>';
+
+                    if ($indice % 2 != 0) {
+                        echo '<div class="separador"></div>';
+                    }
+                }
+            }
+            else{
+                echo '<p>Voçê ainda não possui pacientes cadastrados, para cadastrar clique no botão abaixo</p>';
+                echo '<a href=/pacientes/novo><input class="btn-novo-paciente" type="button" name="" value="Novo"/></a>';
+            }
         echo '</div>';
 ?>
