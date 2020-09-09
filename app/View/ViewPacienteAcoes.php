e5a0c12bb7162b7fa70b07d269ecc8aa14c72a0d
felipeDesenvolvedor <felipedasilvasantos30@gmail.com>
2020-09-09 15:21:50 -0300

    estilo inicial do filtro

--- app/View/ViewPacienteAcoes.php
+++ app/View/ViewPacienteAcoes.php
@@ -9,39 +9,43 @@
                         <a href=/pacientes/novo><input class="btn-novo-paciente" type="button" name="" value="Novo"/></a>
                     </div>
                     <div class="view-filtro-paciente">
-                        <label for="idCpf">
-                            <span> CPF:</span>
-                            <input type="text" name="cpf" id="idCpf">
-                        </label>
+                        <div>
+                            <label for="idCpf">
+                                <span> CPF:</span>
+                                <input type="text" name="cpf" id="idCpf">
+                            </label>
 
-                        <label for="idRG">
-                            <span>RG:</span>            
-                            <input type="text" name="rg" id="idRG">
-                        </label>
+                            <label for="idRG">
+                                <span>RG:</span>            
+                                <input type="text" name="rg" id="idRG">
+                            </label>
+                            
+                            <label for="idResponsavel">
+                                <span>Responsavel: </span>
+                                <input type="text" name="responsavel" id="idResponsavel">
+                            </label>
+                        </div>
                         
-                        <label for="idResponsavel">
-                            <span>Responsavel: </span>
-                            <input type="text" name="responsavel" id="idResponsavel">
-                        </label>
-                        
-                        <label for="idCpfResponsavel">
-                            <span>CPF Responsavel:</span> 
-                            <input type="text" name="cpfResponsavel" id="idCpfResponsavel">
-                        </label>
+                        <div>
+                            <label for="idCpfResponsavel">
+                                <span>CPF Responsavel:</span> 
+                                <input type="text" name="cpfResponsavel" id="idCpfResponsavel">
+                            </label>
 
-                        <label for="idMunicipio">
-                            <span>Municipio</span>
-                            <input type="text" name="municipio" id="idMunicipio">
-                        </label>
+                            <label for="idMunicipio">
+                                <span>Municipio</span>
+                                <input type="text" name="municipio" id="idMunicipio">
+                            </label>
 
-                        <label for="idStatus">
-                            <span>Status</span>
-                            <select name="status" id="idStatus">
-                                <option value="todos">Todos</option>
-                                <option value="0">Inativo</option>
-                                <option value="1">Ativo</option>
-                            </select>
-                        </label>
+                            <label for="idStatus">
+                                <span>Status</span>
+                                <select name="status" id="idStatus">
+                                    <option value="todos">Todos</option>
+                                    <option value="0">Inativo</option>
+                                    <option value="1">Ativo</option>
+                                </select>
+                            </label>
+                        </div>
                     </div>
                 </form>
             </div>    
