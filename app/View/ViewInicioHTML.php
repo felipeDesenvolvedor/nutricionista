dd129fbdd50676fa029b89dfad18be1ec3a6b596
felipeDesenvolvedor <felipedasilvasantos30@gmail.com>
2020-09-14 10:45:01 -0300

    Ajustes de css nos paineis

--- app/View/ViewInicioHTML.php
+++ app/View/ViewInicioHTML.php
@@ -28,43 +28,42 @@
 
     <body>
         <main class="main">
-        <section class="painel-lado-esquerdo">
-                
+            <section class="painel-lado-esquerdo">
+                    
 
-                <div>
-                <div class="usuario">
-                        <div class="foto">
-                            <img class="foto-usuario" src=<?php echo $GLOBALS['caminhoDosArquivos']['FotoUser']?>>
+                    <div>
+                    <div class="usuario">
+                            <div class="foto">
+                                <img class="foto-usuario" src=<?php echo $GLOBALS['caminhoDosArquivos']['FotoUser']?>>
+                            </div>
+                            
+                            <div>
+                                <div class="nome"> Felipe Da Silva Santos</div>
+                                <div class="plano">Plano Bronze</div>
+                            </div>
                         </div>
-                        
-                        <div>
-                            <div class="nome"> Felipe Da Silva Santos</div>
-                            <div class="plano">Plano Bronze</div>
-                        </div>
+                        <ul class="menu-painel"> 
+                            <?php 
+                                foreach($menuPainel as $item)
+                                {
+                                    echo '<li>';
+                                        echo '<a href='.$item['link'].'>';
+                                            echo '<i class="fa '.$item['class'].'"></i>'; 
+                                            echo '<span>'.$item['info'].'</span>';
+                                        echo '</a>';
+
+                                        if(isset($item['subitem']))
+                                        {
+                                            echo '<li>'.$item['subitem']['subItem2'].'</li>';
+                                            echo '<li>'.$item['subitem']['subItem3'].'</li>';
+                                        }
+                                    echo '</li>';
+                                }
+                            ?>
+                        </ul>   
                     </div>
-                    <ul class="menu-painel"> 
-                        <?php 
-                            foreach($menuPainel as $item)
-                            {
-                                echo '<li>';
-                                    echo '<a href='.$item['link'].'>';
-                                        echo '<i class="fa '.$item['class'].'"></i>'; 
-                                        echo '<span>'.$item['info'].'</span>';
-                                    echo '</a>';
-
-                                    if(isset($item['subitem']))
-                                    {
-                                        echo '<li>'.$item['subitem']['subItem2'].'</li>';
-                                        echo '<li>'.$item['subitem']['subItem3'].'</li>';
-                                    }
-                                echo '</li>';
-                            }
-                        ?>
-                    </ul>   
-                </div>
             </section>
-
-                
+                    <?php echo 'teste4';?>
             <section class="painel-lado-direito">
                 <div class="painel-cabecalho">
                     <div class="botao-editar-layput">Editar Layout</div>      
