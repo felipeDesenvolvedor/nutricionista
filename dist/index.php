0bf6c83f74f9e87f6a700568283cb694bd07b956
felipeDesenvolvedor <felipedasilvasantos30@gmail.com>
2020-09-29 13:00:58 -0300

    Access Control: temporario, Modal: posicionamento

--- dist/index.php
+++ dist/index.php
@@ -4,6 +4,8 @@ ini_set("display_errors", 1);
 error_reporting(E_ALL);
 ini_set("display_errors", 1);
 
+header('Access-Control-Allow-Origin: *');
+
 require_once('..'.DIRECTORY_SEPARATOR.'configuracoes'.DIRECTORY_SEPARATOR.'caminhoDosArquivos.php');
 require_once('../configuracoes/configuracoes.php');
 require_once('../src/vendor/autoload.php');
