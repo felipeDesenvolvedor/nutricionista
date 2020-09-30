58917ccfcb81c613d97d0c4e900253711a873bbe
felipeDesenvolvedor <felipedasilvasantos30@gmail.com>
2020-09-30 01:12:49 -0300

    TOPO ANTIGO: removido

--- app/View/ViewInicioHTML.php
+++ app/View/ViewInicioHTML.php
@@ -52,12 +52,7 @@
         </section>
 
         <section class="painel-lado-direito">
-            <div class="painel-cabecalho">
-              <div class="botao-editar-layput">Editar Layout</div>
-              <div class="botao-sair">Sair</div>
-            </div>
-
-
+      
             <div class="painel-corpo">
               <div class="acoes">
                 <h1 class="painel-titulo"><?php echo $this->titulo ?></h1>
