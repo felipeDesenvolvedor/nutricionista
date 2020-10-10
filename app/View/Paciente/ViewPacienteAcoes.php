8cfd654bdd4577d21272e6fc55ab70a19fde6a82
felipeDesenvolvedor <felipedasilvasantos30@gmail.com>
2020-10-10 15:24:30 -0300

    ViewPacienteAcoes

--- app/View/Paciente/ViewPacienteAcoes.php
+++ app/View/Paciente/ViewPacienteAcoes.php
@@ -1,77 +1,69 @@
+<div class='view-filtro'>
+  <form class='view-filtro-paciente-form'>
+    <div>
+      <input class='view-filtro-paciente-busca view-filtro-paciente-campo' type='text' name='nome' placeholder='Pesquisar por nome'/>
+      <a href=/pacientes/novo class='view-filtro-paciente-btn-novo'>Novo</a>
+      <input class='view-filtro-paciente-btn-busca' type='button' name='' value='Buscar'/>
+      <input class='view-filtro-paciente-btn' type='button' name='' value='Filtros'/>
+    </div>
+
+    <div class='view-filtro-paciente'>
+      <div class='view-filtro-paciente-container'>
+        <label for='idCpf' class='view-filtro-paciente-container-campo'>
+          <input type='text' name='cpf' id='idCpf' placeholder='CPF' class='view-filtro-paciente-campo'>
+        </label>
+
+        <label for='idRG' class='view-filtro-paciente-container-campo'>
+          <input type='text' name='rg' id='idRG' placeholder='RG' class='view-filtro-paciente-campo'>
+        </label>
+
+        <label for='idResponsavel' class='view-filtro-paciente-container-campo'>
+          <input type='text' name='responsavel' id='idResponsavel' placeholder='Responsavel' class='view-filtro-paciente-campo'>
+        </label>
+      </div>
+
+      <div class='view-filtro-paciente-container'>
+        <label for='idCpfResponsavel' class='view-filtro-paciente-container-campo'>
+          <input type='text' name='cpfResponsavel' id='idCpfResponsavel' placeholder='CPF Responsavel' class='view-filtro-paciente-campo'>
+        </label>
+        <label for='idStatus' class='view-filtro-paciente-container-campo'>
+          <select name='status' id='idStatus'>
+            <option value='todos'>Status</option>
+            <option value='0'>Inativo</option>
+            <option value='1'>Ativo</option>
+          </select>
+        </label>
+      </div>
+    </div>
+  </form>
+</div>
+
 <?php
-        echo
-        '   <div class="view-filtro">
-                <form class="view-filtro-paciente-form">
-                    <div>
-                        <input class="view-filtro-paciente-busca view-filtro-paciente-campo" type="text" name="nome" placeholder="Pesquisar por nome"/>
-                        <a href=/pacientes/novo class="view-filtro-paciente-btn-novo">Novo</a>
-                        <input class="view-filtro-paciente-btn-busca" type="button" name="" value="Buscar"/>
-                        <input class="view-filtro-paciente-btn" type="button" name="" value="Filtros"/>
-                    </div>
-                    <div class="view-filtro-paciente">
-                        <div class="view-filtro-paciente-container">
-                            <label for="idCpf" class="view-filtro-paciente-container-campo">
-                                <input type="text" name="cpf" id="idCpf" placeholder="CPF" class="view-filtro-paciente-campo">
-                            </label>
+      if(count($this->pacientes)) {
+          echo "<div class='lista-pacientes'>";
 
-                            <label for="idRG" class="view-filtro-paciente-container-campo">
-                                <input type="text" name="rg" id="idRG" placeholder="RG" class="view-filtro-paciente-campo">
-                            </label>
+          foreach($this->pacientes as $paciente)
+          {
+              $inativo = $paciente['status'] ? '' : 'esconder';
 
-                            <label for="idResponsavel" class="view-filtro-paciente-container-campo">
-                                <input type="text" name="responsavel" id="idResponsavel" placeholder="Responsavel" class="view-filtro-paciente-campo">
-                            </label>
-                        </div>
+              echo "<a href=/pacientes/editar/{$paciente['idPaciente']} class='paciente'>";
+                  echo "<div class=lista-paciente-box>";
+                      echo "<div class='lista-paciente-foto'></div>";
 
-                        <div class="view-filtro-paciente-container">
-                            <label for="idCpfResponsavel" class="view-filtro-paciente-container-campo">
-                                <input type="text" name="cpfResponsavel" id="idCpfResponsavel" placeholder="CPF Responsavel" class="view-filtro-paciente-campo">
-                            </label>
-                            <label for="idStatus" class="view-filtro-paciente-container-campo">
-                                <select name="status" id="idStatus">
-                                    <option value="todos">Status</option>
-                                    <option value="0">Inativo</option>
-                                    <option value="1">Ativo</option>
-                                </select>
-                            </label>
-                        </div>
-                    </div>
-                </form>
-            </div>
-        ';
+                      echo "<div class='lista-paciente-dados'>";
+                          echo "<span class='js-lista-paciente-nome'>{$paciente['nome']}</span>";
+                          echo "<span>{$paciente['sexo']}</span>";
+                          echo $paciente['dataNascimento'];
+                          echo "<span class='idade'>{$paciente['dataNascimento']}</span>";
+                      echo "</div>";
+                  echo "</div>";
+              echo "</a>";
+          }
 
-            if(count($this->pacientes)) {
-                echo '<div class="lista-pacientes">';
-                $indice = 0;
-
-                foreach($this->pacientes as $paciente)
-                {
-                    $indice += 1;
-
-                    $inativo = $paciente['status'] ? '' : 'esconder';
-
-                    echo "<a href=/pacientes/editar/{$paciente['idPaciente']} class='paciente'>";
-                        echo '<div class=lista-paciente-box>';
-                            echo '<div class="lista-paciente-foto"></div>';
-
-                            echo '<div class="lista-paciente-dados">';
-                                echo '<span class="js-lista-paciente-nome">'.$paciente['nome'].'</span>';
-                                echo '<span>'.$paciente['sexo'].'</span>';
-                                echo $paciente['dataNascimento'];
-                                echo '<span class="idade">'.$paciente['dataNascimento'].'</span>';
-                            echo '</div>';
-                        echo '</div>';
-                    echo '</a>';
-
-                    // if ($indice % 2 != 0) {
-                    //     echo '<div class="separador"></div>';
-                    // }
-                }
-
-                echo '</div>';
-            }
-            else{
-                echo '<p>Voçê ainda não possui pacientes cadastrados, para cadastrar clique no botão abaixo</p>';
-                echo '<a href=/pacientes/novo><input class="btn-novo-paciente" type="button" name="" value="Novo"/></a>';
-            }
+          echo "</div>";
+      }
+      else{
+          echo "<p>Voçê ainda não possui pacientes cadastrados, para cadastrar clique no botão abaixo</p>";
+          echo "<a href=/pacientes/novo><input class='btn-novo-paciente' type='button' name='' value='Novo'/></a>";
+      }
 ?>
