a56d207a84a2dd2dfbe0c22a46e4eb186ed1b12e
felipeDesenvolvedor <felipedasilvasantos30@gmail.com>
2021-02-11 12:16:33 -0300

    Arquivo Editar  em ES6

--- dist/js/Paciente/Editar.js
+++ dist/js/Paciente/Editar.js
@@ -1,19 +1,20 @@
-$(document).ready(function(){
-  editar();
+$(document).ready(() => {
+  editarPaciente();
 });
 
 
-function editar() {
-  var editarPaciente = "";
-  var idPaciente = 0;
+const editarPaciente = () => {
 
-  $(document).on('click', ".lista-paciente-acao-rapida-editar", function(){
-    editarPaciente = $(this).parents('.paciente');
+  $(document).on('click', ".lista-paciente-acao-rapida-editar", event => {
+    let editarPaciente = "";
+    let idPaciente = 0;
+
+    editarPaciente = $(event.target).parents('.paciente');
     idPaciente = editarPaciente.attr("data-id").split('/');
     idPaciente = idPaciente[idPaciente.length - 1];
 
     window.history.pushState('Object', "Orange Nutri", `http://nutricionista.com.br/pacientes/editar/${idPaciente}`);
 
     buscar("http://nutricionista.com.br/pacientes/editar/", "post", "idPaciente", idPaciente, exibirPagina, "modal");
-  })
+  });
 }
